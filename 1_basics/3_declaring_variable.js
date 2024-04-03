1ba5142f14d33edddc9a278c54613d7abe77eb50
SEUNGHEE <did030930@gmail.com>
2024-04-03 19:58:01 +0900

    Add files via upload

---
+++ 1_basics/3_declaring_variable.js
@@ -0,0 +1,38 @@
+/**
+ * Variable 선언하기
+ * 
+ * 1) var - 더이상 쓰지 않는다. but 굉장히 많음 아직
+ * 2) let
+ * 3) const
+ */
+var name = '코드팩토리';
+console.log(name);
+
+var age = 32;
+console.log(age);
+
+let ive = '아이브';
+console.log(ive);
+
+/**
+ * let과 var로 선언하면
+ * 값을 추후 변경할 수 있다.
+ */
+
+ive = '안유진';
+console.log(ive);
+
+const newJeans = '뉴진스';
+console.log(newJeans);
+
+/**
+ * const로 선언하면 값을 바꿀 수 없음
+ */
+
+/**
+ * 선언과 할당
+ * 
+ * 1) 변수를 선언하는 것.
+ * 2) 할당
+ */
+
---
+++ 1_basics/4_naming_conventions.js
@@ -0,0 +1,28 @@
+/**
+ * Naming Conventions
+ * 
+ * 변수 이름 지을때
+ * 1. 일반적으로 영어를 사용하며 문자와 숫자를 모두 사용 할 수 있다.
+ * 2. 특수기호는 언더스코어와 달러를 사용 할 수 있다.
+ * 3. 숫자로 이름을 시작 할 수 없다
+ *      1Name, 2Hello
+ * 4. 키워드는 변수명으로 사용 할 수 없다.
+ *      var let = 'var';
+ */
+let codefactory = '코드팩토리';
+var $ive = '아이브';
+const _yuJin = '안유진';
+
+console.log(codefactory);
+console.log($ive);
+console.log(_yuJin);
+
+/**
+ * Naming Convention 2
+ * 
+ * 1) camelCase -> 대부분의 언어에서 사용
+ * 2) snake_case -> 파이썬같은 언어에서 사용
+ * 3) PascalCase -> 게임의 언어에서 사용함
+ *  
+ */
+
